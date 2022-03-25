dd19f9206a1daf8711d4d763568cdaac84a8bb32
Ross-Morgan <morgan.ross.512@gmail.com>
2022-03-25 17:31:20 +0000

    commit

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
-class EventLoop:
+from datetime import datetime
+
+
+class Person:
+    def __init__(self, name: str, birth_dt: datetime) -> None:
+        self._name = name
+        self._date_of_birth = birth_dt.date()
+        self._time_of_birth = birth_dt.time()
+        self._birth_dt = birth_dt
+
+
+class Simulator:
     def __init__(self, ips: int) -> None:
-        self.ips = ips
+        self._iterations_per_second = ips
